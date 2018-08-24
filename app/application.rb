2fcb41b5fcddb983c1601ee7e94e2a75a2a64b3a
Christia T <christiatidwell@bellsouth.net>
2018-08-24 18:09:48 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -20,6 +20,7 @@ class Application
       elsif @@cart.empty? != true
       @@cart.each do |purchase|
           resp.write "#{purchase}\n"
+        end
       end
     else
       resp.write "Path Not Found"
